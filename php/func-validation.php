102b4da2a09201173fa0276b52d8d508ed062e2a
Huynh Kien Hao <kienhao2209@gmail.com>
2025-03-18 22:08:13 +0700

    Complete Part 1

--- php/func-validation.php
+++ php/func-validation.php
@@ -1,9 +1,8 @@
 <?php
-
-function is_empty($var, $text, $location, $ms, $data){
-    if ($var){
-
-        $em = "The ".$text." is required";
+function is_empty($var, $text, $location, $ms, $data)
+{
+    if (empty($var)) {
+        $em = "The " . $text . " is required";
         header("Location: $location?$ms=$em&$data");
         exit;
     }
